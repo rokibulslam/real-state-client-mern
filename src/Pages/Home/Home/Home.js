4e2d9e661bea444e347361d9c98fa41593ae0a63
rokibulslam <rokibul1003@gmail.com>
2022-02-11 12:29:39 +0600

    slider

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,15 +4,18 @@ import Featured from '../Featured/Featured';
 import Features from '../Features/Features';
 import Navigation from '../Navigation/Navigation';
 import Parking from '../Parking/Parking';
+import Slide from '../Slide/Slide';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Banner></Banner>
-            <Featured></Featured>
+            
             <Features></Features>
+            
             <Parking></Parking>
+            <Slide></Slide>
         </div>
     );
 };
---
+++ src/Pages/Home/Slide/Slide.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Slider from "react-slick";
+import img1 from '../../../Images/Apartment/slide-one-f-1.jpg'
+import img2 from "../../../Images/Apartment/slide-two.jpg";
+import img3 from "../../../Images/Apartment/slider-2-1-680x510.jpg";
+import './Slide.css'
+
+const Slide = () => {
+   const settings = {
+     dots: true,
+     infinite: true,
+     speed: 500,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+   };
+    return (
+      <div className='py-5 px-sm-2'>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <Slider {...settings}>
+                <div>
+                  <img src={img1} className="img-fluid" alt="" />
+                </div>
+                <div>
+                  <img src={img2} className="img-fluid" alt="" />
+                </div>
+                <div>
+                  <img src={img3} className="img-fluid" alt="" />
+                </div>
+              </Slider>
+            </div>
+            <div className="col-md-6 text-start slide-header">
+              <h1>Amenities</h1>
+              <p>
+                In property and land use planning, amenity is something
+                considered to benefit a location, contribute to its enjoyment,
+                and thereby increase its value.
+              </p>
+              <div className="row li-item">
+                <div className="col-md-6 ">
+                  <ul>
+                    <li>Clubhouse</li>
+                    <li>Fitness Center</li>
+                    <li>Gated</li>
+                    <li>Courtyard</li>
+                    <li>Central Heating</li>
+                    <li>Air Conditioning</li>
+                    <li>Free WiFi</li>
+                  </ul>
+                </div>
+                <div className="col-md-6">
+                  <ul>
+                    <li>Online Services</li>
+                    <li>Health Club Discount</li>
+                    <li>Elevator</li>
+                    <li>Business Center</li>
+                    <li>Alarm System</li>
+                    <li>Car Parking</li>
+                    <li>Greenbelt</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Slide;
