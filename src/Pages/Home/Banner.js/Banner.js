e57fe432d6025f140e12cf8d3f5f76b992848f7f
rokibulslam <rokibul1003@gmail.com>
2022-02-11 10:07:24 +0600

    banner

--- src/Pages/Home/Banner.js/Banner.js
+++ src/Pages/Home/Banner.js/Banner.js
@@ -6,25 +6,22 @@ import './Banner.css'
 const Banner = () => {
  
     return (
-      <div className="banner-bg p-1 d-flex">
-        <div className="container">
-          <div className="row justify-content-between align-content-center">
-            <div className="col-md-6 ">
-              <div></div>
-            </div>
+      <div className="banner-bg d-flex align-items-center">
+        <div className="container ">
+          <div className="row">
+            <div className="col-md-6"></div>
             <div className="col-md-6 ">
               <div className="text-start banner-text rounded">
                 <p>
                   <u>Aragon Ave, Coral Gables, FL 33134</u>
-                  <h1>Gables Grand Plaza</h1>
-                  <h1>Apartments</h1>
-                  <p>
-                    Our premium furnished apartments feature allow you and your
+                  <h1 className='pt-4'>Gables Grand Plaza <br/> Apartments</h1>
+                  <p className='py-4'>
+                    Our premium furnished apartments feature allow you and your <br/> 
                     favorite pet to enjoy all at Gables Grand Plaza!
                   </p>
-                  <div>
-                    <h1>$625,000</h1>
-                    <button className="banner-btn">Buy Apartment</button>
+                  <div className='row'>
+                    <h1 className='col-md-6'>$625,000</h1>
+                    <button className="banner-btn col-md-6">Buy Apartment</button>
                   </div>
                 </p>
               </div>
--- src/Pages/Home/Features/Features.js
+++ src/Pages/Home/Features/Features.js
@@ -1,47 +1,73 @@
 import React from 'react';
-
+ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+ 
 const Features = () => {
     return (
-      <div className="my-5 py-5">
+      <div className="my-5 py-5 text-start">
         <div className="container">
           <div className="row">
             <div className="col-md-4">
               <p>GREAT FEATURES</p>
-              <h1>Classic Interiors &</h1>
-              <h1>Exteriors</h1>
+              <h1>
+                Classic Interiors & <br />
+                Exteriors
+              </h1>
               <p>
-                Generously sized, this home delivers the very best Interiors &
-                exteriors.
+                Generously sized, this home delivers the very <br /> best
+                Interiors & exteriors.
               </p>
               <button className="banner-btn">Buy Apartment</button>
             </div>
             <div className="col-md-8">
               <div className="row">
                 <div className="col-md-6">
-                  <h1>Latest Property</h1>
+                  <div className="d-flex flex-column">
+                    <span>
+                      <FontAwesomeIcon icon={faHome} size="4x" />
+                    </span>
+                    <h1>Latest Property</h1>
+                  </div>
                   <p>
-                    Tasteful and seem-less renovation, this home is a unique
-                    refreshing
+                    Tasteful and seem-less renovation,
+                    <br /> this home is a unique refreshing
                   </p>
                 </div>
                 <div className="col-md-6">
-                  <h1>Awesome Area</h1>
+                  <div className="d-flex flex-column">
+                    <span>
+                      <FontAwesomeIcon icon={faHome} size="4x" />
+                    </span>
+                    <h1>Latest Property</h1>
+                  </div>
                   <p>
-                    Tasteful and seem-less renovation, this home is a unique
-                    refreshing
+                    Tasteful and seem-less renovation,
+                    <br /> this home is a unique refreshing
                   </p>
                 </div>
                 <div className="col-md-6">
-                  <h1>Great Surroundings</h1>
+                  <div className="d-flex flex-column">
+                    <span>
+                      <FontAwesomeIcon icon={faHome} size="4x" />
+                    </span>
+                    <h1>Latest Property</h1>
+                  </div>
                   <p>
-                    Tasteful and seem-less renovation, this home is a unique
-                    refreshing
+                    Tasteful and seem-less renovation, <br />
+                    this home is a unique refreshing
                   </p>
                 </div>
                 <div className="col-md-6">
-                  <h1>Supreme Location</h1>
-                  Tasteful and seem-less renovation, this home is a unique
-                  refreshing
+                  <div className="d-flex flex-column">
+                    <span>
+                      <FontAwesomeIcon icon={faHome} size="4x" />
+                    </span>
+                    <h1>Latest Property</h1>
+                  </div>
+                  <p>
+                    Tasteful and seem-less renovation,
+                    <br /> this home is a unique refreshing
+                  </p>
                 </div>
               </div>
             </div>
