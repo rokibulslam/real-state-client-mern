a0d511dfee712377dcab62ae755db5a1c274333b
rokibulslam <rokibul1003@gmail.com>
2022-02-11 11:51:09 +0600

    responsive

--- src/Pages/Home/Features/Features.js
+++ src/Pages/Home/Features/Features.js
@@ -6,84 +6,94 @@ const Features = () => {
     return (
       <div className="my-5 py-5 text-start features d-flex justify-content-center align-items-center">
         <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-4">
-              <p>GREAT FEATURES</p>
-              <h1>
-                Classic Interiors & <br />
-                Exteriors
-              </h1>
-              <p className="pt-4">
-                Generously sized, this home delivers the very <br /> best
-                Interiors & exteriors.
-              </p>
-              <button className="banner-btn my-5">Buy Apartment</button>
+          <div className="row">
+            <div className="col-md-4 d-flex flex-column justify-content-center align-items-center">
+              <div>
+                <p>GREAT FEATURES</p>
+                <h1>
+                  Classic Interiors & <br />
+                  Exteriors
+                </h1>
+                <p className="pt-4">
+                  Generously sized, this home delivers the very <br /> best
+                  Interiors & exteriors.
+                </p>
+                <button className="banner-btn my-5">Buy Apartment</button>
+              </div>
             </div>
-            <div className="col-md-8">
+            <div className="col-md-8 ">
               <div className="row ">
-                <div className="col-md-6 mb-3">
-                  <div className="d-flex flex-column">
-                    <span>
-                      <FontAwesomeIcon
-                        className="features-icon"
-                        icon={faHome}
-                        size="4x"
-                      />
-                    </span>
-                    <h1>Latest Property</h1>
+                <div className="col-md-6 mb-3 d-flex justify-content-center align-items-center">
+                  <div>
+                    <div className="d-flex flex-column">
+                      <span>
+                        <FontAwesomeIcon
+                          className="features-icon"
+                          icon={faHome}
+                          size="4x"
+                        />
+                      </span>
+                      <h1>Latest Property</h1>
+                    </div>
+                    <p>
+                      Tasteful and seem-less renovation,
+                      <br /> this home is a unique refreshing
+                    </p>
                   </div>
-                  <p>
-                    Tasteful and seem-less renovation,
-                    <br /> this home is a unique refreshing
-                  </p>
                 </div>
-                <div className="col-md-6 mb-3">
-                  <div className="d-flex flex-column">
-                    <span>
-                      <FontAwesomeIcon
-                        className="features-icon"
-                        icon={faHome}
-                        size="4x"
-                      />
-                    </span>
-                    <h1>Latest Property</h1>
+                <div className="col-md-6 mb-3 d-flex justify-content-center align-items-center">
+                  <div>
+                    <div className="d-flex flex-column">
+                      <span>
+                        <FontAwesomeIcon
+                          className="features-icon"
+                          icon={faHome}
+                          size="4x"
+                        />
+                      </span>
+                      <h1>Latest Property</h1>
+                    </div>
+                    <p>
+                      Tasteful and seem-less renovation,
+                      <br /> this home is a unique refreshing
+                    </p>
                   </div>
-                  <p>
-                    Tasteful and seem-less renovation,
-                    <br /> this home is a unique refreshing
-                  </p>
                 </div>
-                <div className="col-md-6">
-                  <div className="d-flex flex-column">
-                    <span>
-                      <FontAwesomeIcon
-                        className="features-icon"
-                        icon={faHome}
-                        size="4x"
-                      />
-                    </span>
-                    <h1>Latest Property</h1>
+                <div className="col-md-6 mb-3 d-flex justify-content-center align-items-center">
+                  <div>
+                    <div className="d-flex flex-column">
+                      <span>
+                        <FontAwesomeIcon
+                          className="features-icon"
+                          icon={faHome}
+                          size="4x"
+                        />
+                      </span>
+                      <h1>Latest Property</h1>
+                    </div>
+                    <p>
+                      Tasteful and seem-less renovation,
+                      <br /> this home is a unique refreshing
+                    </p>
                   </div>
-                  <p>
-                    Tasteful and seem-less renovation, <br />
-                    this home is a unique refreshing
-                  </p>
                 </div>
-                <div className="col-md-6">
-                  <div className="d-flex flex-column">
-                    <span>
-                      <FontAwesomeIcon
-                        className="features-icon"
-                        icon={faHome}
-                        size="4x"
-                      />
-                    </span>
-                    <h1>Latest Property</h1>
+                <div className="col-md-6 mb-3 d-flex justify-content-center align-items-center">
+                  <div>
+                    <div className="d-flex flex-column">
+                      <span>
+                        <FontAwesomeIcon
+                          className="features-icon"
+                          icon={faHome}
+                          size="4x"
+                        />
+                      </span>
+                      <h1>Latest Property</h1>
+                    </div>
+                    <p>
+                      Tasteful and seem-less renovation,
+                      <br /> this home is a unique refreshing
+                    </p>
                   </div>
-                  <p>
-                    Tasteful and seem-less renovation,
-                    <br /> this home is a unique refreshing
-                  </p>
                 </div>
               </div>
             </div>
